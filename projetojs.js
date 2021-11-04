a66c5f0f706aff8ccc60649218f7663f48c9fad0
Renata Pulz <84743054+renatapulz@users.noreply.github.com>
2021-11-04 16:55:51 -0300

    corrigido imagens

--- projetojs.js
+++ projetojs.js
@@ -1,211 +1,196 @@
 // acessibilidade - texto
-let tamanho = 26;
+let tamanho = 26
 function diminuir() {
-    tamanho --;
-    document.body.style.fontSize = tamanho +"px";
+  tamanho--
+  document.body.style.fontSize = tamanho + 'px'
 }
 function aumentar() {
-    tamanho ++;
-    document.body.style.fontSize = tamanho +"px";
+  tamanho++
+  document.body.style.fontSize = tamanho + 'px'
 }
-
-
-
 
 // slider
-let intervalo = 3000;
-function slide1(){
-    document.getElementById("banner").src ="https://firebasestorage.googleapis.com/v0/b/kinetic-scheme-317614.appspot.com/o/banner1.png?alt=media&token=ea5b3b39-e114-4a11-9e78-62e402636e06";
-    setTimeout("slide2()",intervalo);
+let intervalo = 3000
+function slide1() {
+  document.getElementById('banner').src = 'img/banner1.jpg'
+  setTimeout('slide2()', intervalo)
 }
 
-function slide2(){
-    document.getElementById("banner").src ="https://firebasestorage.googleapis.com/v0/b/kinetic-scheme-317614.appspot.com/o/banner2_0.png?alt=media&token=ebc99fd3-bfb9-40f0-a5bb-2c11b8f0001c";
-    setTimeout("slide3()",intervalo);
+function slide2() {
+  document.getElementById('banner').src = 'img/banner2.jpg'
+  setTimeout('slide3()', intervalo)
 }
 
-function slide3(){
-    document.getElementById("banner").src ="https://firebasestorage.googleapis.com/v0/b/kinetic-scheme-317614.appspot.com/o/banner3.png?alt=media&token=94cd246e-a279-473c-9be2-a8f71e245812";
-    setTimeout("slide1()",intervalo);
+function slide3() {
+  document.getElementById('banner').src = 'img/banner3.jpg'
+  setTimeout('slide1()', intervalo)
 }
-
-
-
-
 
 // botao dark mode - ativar e desativar:
 
 const escurecer = () => {
-    if (document.getElementById('tudo').classList.contains('dark')) {
-        document.getElementById('tudo').className = "";
-    }
-    else{
-        document.getElementById('tudo').className = "dark";
-    }
+  if (document.getElementById('tudo').classList.contains('dark')) {
+    document.getElementById('tudo').className = ''
+  } else {
+    document.getElementById('tudo').className = 'dark'
+  }
 }
-
 
 // PAGINA CONVERSAO:
 
 const resetar = () => {
-    
-    document.getElementById('valoremreal').value = "";
-    document.getElementById('resultvalor').innerHTML = "";
+  document.getElementById('valoremreal').value = ''
+  document.getElementById('resultvalor').innerHTML = ''
 }
 
 const resetar2 = () => {
-    
-    document.getElementById('valoremcelsius').value = "";
-    document.getElementById('resultemp').innerHTML = "";
+  document.getElementById('valoremcelsius').value = ''
+  document.getElementById('resultemp').innerHTML = ''
 }
-
 
 // conversor de real:
 const converterreal = () => {
-    let resultadovalor = 0;
-    const real = parseFloat(document.getElementById("valoremreal").value);
-    const opcmoeda = document.getElementById("opcaomoeda").value;
-    if (isNaN(real)) {
-        alert('Digite um valor em real');
-        } else{
-            if(opcmoeda == "d"){
-                resultadovalor= real / (5.06); 
-            }else {
-                resultadovalor = real / 6;
-            }
-        }
+  let resultadovalor = 0
+  const real = parseFloat(document.getElementById('valoremreal').value)
+  const opcmoeda = document.getElementById('opcaomoeda').value
+  if (isNaN(real)) {
+    alert('Digite um valor em real')
+  } else {
+    if (opcmoeda == 'd') {
+      resultadovalor = real / 5.06
+    } else {
+      resultadovalor = real / 6
+    }
+  }
 
-    document.getElementById('resultvalor').innerHTML =  ` ${resultadovalor.toFixed(2)}`;
+  document.getElementById('resultvalor').innerHTML = ` ${resultadovalor.toFixed(
+    2
+  )}`
 }
-
-
-
 
 // conversor de temperatura:
 const convertertemp = () => {
-    let  resultadotemp = 0;
-    const temp = parseFloat(document.getElementById("valoremcelsius").value);
-    const opctemp = document.getElementById("opcaotemp").value;
-        if (isNaN(temp)) {
-            alert('Digite um valor para temperatura');
-            } else{
-                 if(opctemp == "f"){
-            resultadotemp = ((temp * 9/5) + 32); 
-                }else {
-                    resultadotemp = temp + 273;
-                }
-            }
-
-        document.getElementById('resultemp').innerHTML = ` ${resultadotemp.toFixed(2)}`;
+  let resultadotemp = 0
+  const temp = parseFloat(document.getElementById('valoremcelsius').value)
+  const opctemp = document.getElementById('opcaotemp').value
+  if (isNaN(temp)) {
+    alert('Digite um valor para temperatura')
+  } else {
+    if (opctemp == 'f') {
+      resultadotemp = (temp * 9) / 5 + 32
+    } else {
+      resultadotemp = temp + 273
     }
+  }
 
-
-
+  document.getElementById('resultemp').innerHTML = ` ${resultadotemp.toFixed(
+    2
+  )}`
+}
 
 // calculadora:
 const limpar = () => {
-    document.getElementById('a').value = "";
-    document.getElementById('b').value = "";
-    document.getElementById('result').innerHTML = "Resultado";
+  document.getElementById('a').value = ''
+  document.getElementById('b').value = ''
+  document.getElementById('result').innerHTML = 'Resultado'
 }
 
-const calcular = (operacao) => {
-    const valorA = parseFloat(document.getElementById("a").value);
-    const valorB = parseFloat(document.getElementById("b").value);
-    let resultado = '';
+const calcular = operacao => {
+  const valorA = parseFloat(document.getElementById('a').value)
+  const valorB = parseFloat(document.getElementById('b').value)
+  let resultado = ''
 
-    switch (operacao) {
-        case 'somar':
-            resultado = somar(valorA, valorB);
-            break;
-        case 'subtrair':
-            resultado = subtrair(valorA, valorB);
-            break;
-        case 'multiplicar':
-            resultado = multiplicar(valorA, valorB);
-            break;
-        case 'dividir':
-            resultado = dividir(valorA, valorB);
-            break;  
-        case 'porcentagem':
-            resultado = porcentagem(valorA, valorB);
-            break;  
-        case 'aelevado2':
-            resultado = aelevado2(valorA);
-            break; 
-        case 'belevado2':
-            resultado = belevado2(valorB);
-            break; 
-        case 'aelevadob':
-            resultado = aelevadob(valorA, valorB);
-            break;     
-            
-    }
+  switch (operacao) {
+    case 'somar':
+      resultado = somar(valorA, valorB)
+      break
+    case 'subtrair':
+      resultado = subtrair(valorA, valorB)
+      break
+    case 'multiplicar':
+      resultado = multiplicar(valorA, valorB)
+      break
+    case 'dividir':
+      resultado = dividir(valorA, valorB)
+      break
+    case 'porcentagem':
+      resultado = porcentagem(valorA, valorB)
+      break
+    case 'aelevado2':
+      resultado = aelevado2(valorA)
+      break
+    case 'belevado2':
+      resultado = belevado2(valorB)
+      break
+    case 'aelevadob':
+      resultado = aelevadob(valorA, valorB)
+      break
+  }
 
-    document.getElementById('result').innerHTML = resultado;
+  document.getElementById('result').innerHTML = resultado
 }
 
 const somar = (a, b) => {
-    if (isNaN(a) || isNaN(b)) {
-        alert('Verifique os valores nos campos A e B');
-        return "Resultado";
-    }
-    return a + b;
+  if (isNaN(a) || isNaN(b)) {
+    alert('Verifique os valores nos campos A e B')
+    return 'Resultado'
+  }
+  return a + b
 }
 
 const subtrair = (a, b) => {
-    if (isNaN(a) || isNaN(b)) {
-        alert('Verifique os valores nos campos A e B');
-        return "Resultado";
-    }
-    return a - b;
+  if (isNaN(a) || isNaN(b)) {
+    alert('Verifique os valores nos campos A e B')
+    return 'Resultado'
+  }
+  return a - b
 }
 
 const multiplicar = (a, b) => {
-    if (isNaN(a) || isNaN(b)) {
-        alert('Verifique os valores nos campos A e B');
-        return "Resultado";
-    }
-    return a * b;
+  if (isNaN(a) || isNaN(b)) {
+    alert('Verifique os valores nos campos A e B')
+    return 'Resultado'
+  }
+  return a * b
 }
 
 const dividir = (a, b) => {
-    if (isNaN(a) || isNaN(b)) {
-        alert('Verifique os valores nos campos A e B');
-        return "Resultado";
-    }
-    return a / b;
+  if (isNaN(a) || isNaN(b)) {
+    alert('Verifique os valores nos campos A e B')
+    return 'Resultado'
+  }
+  return a / b
 }
 
 const porcentagem = (a, b) => {
-    if (isNaN(a) || isNaN(b)) {
-        alert('Verifique os valores nos campos A e B');
-        return "Resultado";
-    }
-    return (a*b)/100;
+  if (isNaN(a) || isNaN(b)) {
+    alert('Verifique os valores nos campos A e B')
+    return 'Resultado'
+  }
+  return (a * b) / 100
 }
 
-const aelevado2 = (a) => {
-    if (isNaN(a)) {
-        alert('Verifique os valores no campo A');
-        return "Resultado";
-    }
-    return a*a;
+const aelevado2 = a => {
+  if (isNaN(a)) {
+    alert('Verifique os valores no campo A')
+    return 'Resultado'
+  }
+  return a * a
 }
 
-const belevado2 = (b) => {
-    if (isNaN(b)) {
-        alert('Verifique os valores no campo B');
-        return "Resultado";
-    }
-    return b*b;
+const belevado2 = b => {
+  if (isNaN(b)) {
+    alert('Verifique os valores no campo B')
+    return 'Resultado'
+  }
+  return b * b
 }
 
 const aelevadob = (a, b) => {
-    if (isNaN(a) || isNaN(b)) {
-        alert('Verifique os valores nos campos A e B');
-        return "Resultado";
-    }
+  if (isNaN(a) || isNaN(b)) {
+    alert('Verifique os valores nos campos A e B')
+    return 'Resultado'
+  }
 
-    return Math.pow(a, b);
+  return Math.pow(a, b)
 }
